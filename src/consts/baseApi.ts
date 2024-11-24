b4492f15ec70a535a23db9ef44161549a9b36dcb
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-11-24 17:11:52 -0500

    se coloca la ip del servidor

--- src/consts/baseApi.ts
+++ src/consts/baseApi.ts
@@ -1,1 +1,2 @@
-export const baseUrlApi = "https://api-revista.previser.com.co/api";
+// export const baseUrlApi = "https://api-revista.previser.com.co/api";
+export const baseUrlApi = "http://192.168.92.248:3001/api";
