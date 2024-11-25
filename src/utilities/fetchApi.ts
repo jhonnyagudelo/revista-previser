db435f2f0adee9b81f36e6c2bdc599902cbf7a8b
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-11-25 10:28:50 -0500

    api

--- src/utilities/fetchApi.ts
+++ src/utilities/fetchApi.ts
@@ -6,7 +6,7 @@ export const fetchApi = async <T>(
   endpoint: string,
   options?: RequestInit
 ): Promise<T> => {
-  const url = `${baseUrlLocal}${endpoint}`;
+  const url = `${baseUrlApi}${endpoint}`;
   const response = await fetch(url, options);
 
   if (!response.ok) {
