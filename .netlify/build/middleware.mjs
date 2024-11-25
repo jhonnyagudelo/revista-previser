213364ea464f61f8ca4990a5bcde1ed4dfddd8e8
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-11-25 08:23:27 -0500

    se cambia el mensaje en attedance

--- .netlify/build/middleware.mjs
+++
@@ -1,5 +0,0 @@
-import 'es-module-lexer';
-import './chunks/shared_BARcEl2T.mjs';
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-export { onRequest } from './_astro-internal_middleware.mjs';
--- .netlify/v1/functions/ssr/.netlify/build/chunks/index_dB0aRVUn.mjs
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
-
-export { prisma as p };
