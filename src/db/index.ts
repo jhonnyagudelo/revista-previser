e9cb9a1e953035f33ce1f83b2033d5752671fc42
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2025-11-18 10:06:59 -0500

    cambia la ruta del config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import * as dotenv from "dotenv";
 
 // Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
