4f90c0528a3d609ce6b47529528be22de10fbb79
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2025-11-18 08:55:05 -0500

    se configura vite para que no bloquee

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,9 +12,18 @@ export default defineConfig({
   site: "https://revista.previser.com.co",
   integrations: [react(), tailwind()],
   output: "server",
+  // Configuración de Astro (y del servidor Vite que usa)
   server: {
     port: 4321, // Cambia al puerto que desees
     host: true, // Escucha en todas las interfaces de red
+  },
+  // Configuración de Vite expuesta vía Astro
+  vite: {
+    server: {
+      host: "0.0.0.0",
+      // Agrega los hostnames permitidos aquí:
+      allowedHosts: ["revista.previser.com.co", "localhost", "127.0.0.1"],
+    },
   },
 
   build: {
